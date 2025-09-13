b5d5b6e1294bd5096ffdf2e168d7c2bd6241c0ae
Labeya <laurentmwn@gmail.com>
2025-09-13 16:45:01 +0100

    create controller and define routes for welcome page

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+Route::get('/', \App\Http\Controllers\WelcomeController::class)->name('home');
