318cf70ae2d2f54690b8484f5e3e9fbe29a32bd7
Labeya <laurentmwn@gmail.com>
2025-09-23 07:27:11 +0100

    update style for components shared/welcome/*

--- resources/js/shared/welcome/activities.tsx
+++ resources/js/shared/welcome/activities.tsx
@@ -3,13 +3,17 @@ import { Activity } from '@/types/model';
 import React from 'react';
 import { ActivityCollection } from '../activity/activity-card';
 
-type Props = { activities: Activity[] };
+type Props = {
+    activities: Activity[];
+};
 
 export const WelcomeActivities: React.FC<Props> = ({ activities }) => {
-    if (activities.length === 0) return null;
+    if (!activities.length) {
+        return null;
+    }
 
     return (
-        <div className="container py-10 lg:py-12">
+        <div className="container py-10 lg:py-12" aria-label="Section des dernières activités">
             <HeadingLarge title="Dernières activités" className="mb-8 text-center">
                 Découvrez en un clin d’œil les dernières opportunités partagées par nos recruteurs et donnez
                 <br />
@@ -19,7 +23,7 @@ export const WelcomeActivities: React.FC<Props> = ({ activities }) => {
             <div className="mx-auto grid max-w-5xl grid-cols-1 items-stretch gap-6 lg:grid-cols-2">
                 {activities.map((activity) => (
                     <div key={activity.id} className="h-full">
-                        <ActivityCollection  activity={activity} />
+                        <ActivityCollection activity={activity} />
                     </div>
                 ))}
             </div>
